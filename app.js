e7a576bfd8873f04f892332b75d757e6d1e601b6
camila <camiramirezpp@gmail.com>
2018-05-26 19:22:15 -0400

    codigo del js

---
+++ app.js
@@ -0,0 +1,18 @@
+let numero1 = 9824;
+let numero2 = 777;
+let numero3 = 36325;
+
+
+const add = (numero1 + numero2);
+const result = (add * numero3);
+
+button.addEventListener('click', function(){
+    //txtDos.innerHTML = '';
+    resultado.innerHTML = (result);
+})
+
+
+
+
+
+ 
